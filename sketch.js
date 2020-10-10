75094dcca088c6c4d3af45b453c77fa37e646387
solipsia <63716242+solipsia@users.noreply.github.com>
2020-10-10 16:32:08 +0100

    .

--- sketch.js
+++ sketch.js
@@ -14,24 +14,6 @@ function addCommas(nStr) {
     return x1 + x2;
 }
 
-function formatM(number, decPlaces, decSep, thouSep) {
-	decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces,
-	decSep = typeof decSep === "undefined" ? "." : decSep;
-	thouSep = typeof thouSep === "undefined" ? "," : thouSep;
-	var sign = number < 0 ? "-" : "";
-	var i = String(parseInt(number = Math.abs(Number(number) || 0).toFixed(decPlaces)));
-	var j = (j = i.length) > 3 ? j % 3 : 0;
-	console.log(i,j,i.length,j % 3);
-	return sign +
-		(j ? i.substr(0, j) + thouSep : "") +
-		i.substr(j).replace(/(\decSep{3})(?=\decSep)/g, "$1" + thouSep) +
-		(decPlaces ? decSep + Math.abs(number - i).toFixed(decPlaces).slice(2) : "");
-	}
-	
-	document.getElementById("b").addEventListener("click", event => {
-	  document.getElementById("x").innerText = "Result was: " + formatMoney(document.getElementById("d").value);
-	});
-
 function draw() {
 	background(0);
 	let baseline=530000;
@@ -44,10 +26,5 @@ function draw() {
 	textSize(150);
 	fill(255, 255, 255);
 	textAlign(CENTER, CENTER);
-	//text(formatM(currentvalue,2,'.',','),width/2, height/2);
-	
 	text(addCommas(outputstr),width/2, height/2);
-	//text(addCommas(1234),width/2, height/2);
-
-
 }
