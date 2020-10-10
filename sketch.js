6eff2411f1bceaf9da8641257efb14d868086e9c
solipsia <63716242+solipsia@users.noreply.github.com>
2020-10-10 16:28:22 +0100

    .

--- sketch.js
+++ sketch.js
@@ -2,6 +2,18 @@ function setup() {
 	createCanvas(windowWidth, windowHeight);
 }
 
+function addCommas(nStr) {
+    nStr += '';
+    var x = nStr.split('.');
+    var x1 = x[0];
+    var x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+            x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    return x1 + x2;
+}
+
 function formatM(number, decPlaces, decSep, thouSep) {
 	decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces,
 	decSep = typeof decSep === "undefined" ? "." : decSep;
@@ -9,7 +21,7 @@ function formatM(number, decPlaces, decSep, thouSep) {
 	var sign = number < 0 ? "-" : "";
 	var i = String(parseInt(number = Math.abs(Number(number) || 0).toFixed(decPlaces)));
 	var j = (j = i.length) > 3 ? j % 3 : 0;
-	
+	console.log(i,j,i.length,j % 3);
 	return sign +
 		(j ? i.substr(0, j) + thouSep : "") +
 		i.substr(j).replace(/(\decSep{3})(?=\decSep)/g, "$1" + thouSep) +
@@ -32,5 +44,10 @@ function draw() {
 	textSize(100);
 	fill(255, 255, 255);
 	textAlign(CENTER, CENTER);
-	text(formatM(currentvalue,2,'.',','),width/2, height/2);
+	//text(formatM(currentvalue,2,'.',','),width/2, height/2);
+	
+	text(addCommas(outputstr),width/2, height/2);
+	//text(addCommas(1234),width/2, height/2);
+
+
 }
